7613e494c97a95ace3938ee255bc90ae75cfbf2d
Bimal Kumar Sah <58667490+Bimal27@users.noreply.github.com>
2023-05-14 17:46:24 +0200

    Update Featured.jsx

--- src/component/Featured/Featured.jsx
+++ src/component/Featured/Featured.jsx
@@ -5,7 +5,7 @@ const Featured = () => {
     <div className="featured">
       <div className="featuredItem">
         <img
-          src="https://t-cf.bstatic.com/xdata/images/city/540x270/972591.webp?k=0730f8d7129afb9ba91bd055c801b061fc778dd1f5f82cfcc911e45e09703c64&amp;o="
+          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
           alt=""
           className="featuredImg"
         />
@@ -28,7 +28,7 @@ const Featured = () => {
       </div>
       <div className="featuredItem">
         <img
-          src="https://t-cf.bstatic.com/xdata/images/city/540x270/972611.webp?k=ad717a318610993a0d25cc1938fc17106d7a07b3489b89e239489f5e943756dd&amp;o="
+          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
           alt=""
           className="featuredImg"
         />
